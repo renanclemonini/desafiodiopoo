e0b6e5273019d047251df9765c4223fc808bbf79
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2023-04-19 17:53:29 -0300

    herança e polimorfismo pt2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -5,9 +6,9 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso1 = new Curso("Curso de Java", "texto descrição",60);
-        Curso curso2 = new Curso("Curso de PHP", "texto descrição",40);
-        Mentoria mentoria1 = new Mentoria("Mentoria em JAVA","descrição da mentoria em JAVA", LocalDate.now());
+        Conteudo curso1 = new Curso("Curso de Java", "texto descrição",60);
+        Conteudo curso2 = new Curso("Curso de PHP", "texto descrição",40);
+        Conteudo mentoria1 = new Mentoria("Mentoria em JAVA","descrição da mentoria em JAVA", LocalDate.now());
 
 
         System.out.println(curso1);
