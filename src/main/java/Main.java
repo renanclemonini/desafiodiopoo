67eaa43e9ecf076edca921d2114a99a3f62b27f5
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2023-04-19 17:13:35 -0300

    herança e polimorfismo pt1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso("Curso de Java", "texto descrição",60);
         Curso curso2 = new Curso("Curso de PHP", "texto descrição",40);
+        Mentoria mentoria1 = new Mentoria("Mentoria em JAVA","descrição da mentoria em JAVA", LocalDate.now());
 
-        Mentoria mentoria1 = new Mentoria("Mentoria de Java", "descrição da mentoria de java", LocalDate.now());
 
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria1);
+
 
     }
 }
--- src/main/java/br/com/dio/desafio/dominio/Curso.java
+++ src/main/java/br/com/dio/desafio/dominio/Curso.java
@@ -1,33 +1,20 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
     public Curso() {
     }
 
     public Curso(String titulo, String descricao, int cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
+        setTitulo(titulo);
+        setDescricao(descricao);
         this.cargaHoraria = cargaHoraria;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
@@ -41,8 +28,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
